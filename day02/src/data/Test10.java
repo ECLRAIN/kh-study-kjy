9bf7ed1a3fa61b8e47059c982e1459837348ff60
ECLRAIN <irlawhddus@gmail.com>
2022-07-26 16:58:24 +0900

    실수에 대해서

---
+++ day02/src/data/Test10.java
@@ -0,0 +1,22 @@
+package data;
+import java.lang.*;
+public class Test10 {
+	public static void main(String[] args) {
+		//실수의 종류와 특징
+		//실수는 소수점이 있는 숫자
+		
+		//-float - 2^32(4byte, 32bit)
+		//-double - 기본값
+		
+		float a = 1.5f;
+		double b = 1.5;
+		System.out.println(a);
+		System.out.println(b);
+		
+		float c = 1.25484241894165184564f;
+		System.out.println(c);
+		double d = 1.2348748444784784848498498;
+		System.out.println(d);
+	}
+
+}

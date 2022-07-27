82d171379412e445599f09acf89bd527990cf28d
ECLRAIN <irlawhddus@gmail.com>
2022-07-27 18:23:17 +0900

    0727과제

---
+++ day03/src/condition/Test07.java
@@ -0,0 +1,17 @@
+package condition;
+import java.lang.*;
+public class Test07 {
+	public static void main(String [] args) {
+		//인원 수= 2명, 여행기간 =2일 여행 계획중인 달 6월
+		//678에 여행가는 사람은 25퍼할인
+		//예상 여행 경비를 계산하여 출력하도록 
+		int human=2;
+		int day=2;
+		int month= 6;
+		int dayilyprice=100000;
+		//계산
+		int party=human+day;
+		int price=dayilyprice * 75/100;
+		
+	}
+}

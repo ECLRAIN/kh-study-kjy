1975642fd99ab81f0cac4deafaf8689c3035aba9
ECLRAIN <irlawhddus@gmail.com>
2022-07-27 14:48:05 +0900

    조건 예제

--- day03/src/condition/Test01.java
+++ day03/src/condition/Test01.java
@@ -2,14 +2,25 @@ package condition;
 import java.lang.*;
 public class Test01{
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
-		
-		if(odd==true)
-		if(even==true)
-	}
-}
+		//조건부 코드
+				//- 특정 상황에서만 실행되는 코드
+
+				//준비
+				int number = 6;
+
+				//계산
+				int mod = number % 2;
+				//boolean odd = 나머지가 1이면;
+				boolean odd = mod == 1;
+				//boolean even = 나머지가 0이면;
+				boolean even = mod == 0;
+
+				//출력
+				if(odd == true){//이 영역은 odd라는 변수가 true일 때 실행되어야 한다
+					System.out.println(number + "는 홀수입니다");
+				}
+				if(even == true){//이 영역은 even이라는 변수가 true일 때 실행되어야 한다
+					System.out.println(number + "는 짝수입니다");
+				}
+			}
+		}

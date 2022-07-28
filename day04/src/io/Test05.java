f8b8eeac2e3f635b7cf6fa23c0c5452577dfc42d
ECLRAIN <irlawhddus@gmail.com>
2022-07-28 17:01:43 +0900

    //Scanner sc=new Scanner, 더블

---
+++ day04/src/io/Test05.java
@@ -0,0 +1,25 @@
+package io;
+import java.lang.*;
+import java.util.Scanner;
+public class Test05 {
+	public static void main(String[] args) {
+		//준비
+		double cm=180;
+		double kg = 80;
+		//계산
+		double m= cm/100;
+		double bmi=kg/(m*m);
+		//출력
+		if(bmi>)
+		
+		
+		//Scanner sc=new Scanner(System.in);
+		//System.out.println("체중을 입력하세요");
+		//int kg=sc.nextInt();
+		//System.out.println("키를 입력하세요");
+		//int cm=sc.nextInt();
+		//sc.close();
+		
+		//int bmi=kg/cm*cm/100
+	}
+}

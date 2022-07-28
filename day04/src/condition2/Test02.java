56eae169a670970391577be4a2dd65e69603bb85
ECLRAIN <irlawhddus@gmail.com>
2022-07-28 17:32:17 +0900

    스위치 케이스

---
+++ day04/src/condition2/Test02.java
@@ -0,0 +1,26 @@
+package condition2;
+import java.lang.*;
+public class Test02 {
+	public static void main(String[] args) {
+	//t01을 스위치 케이스 구문으로 변경
+		//if문은 한권씩 찾고
+		//스위치는 한권으로 묶어서
+int direction =4;
+		
+		switch(direction==2) {
+		case 2: System.out.println("아래로 한 칸 이동");
+		
+		case : System.out.println(
+			System.out.println("왼쪽으로 한 칸 이동");
+		
+		else if(direction==6) 
+			System.out.println("오른쪽으로 한 칸 이동");
+		
+		else if(direction==8) 
+			System.out.println("위쪽으로 한 칸 이동");
+		
+		else 
+			System.out.println("잘못된 입력");
+		}
+	}
+}

8b6a8f2ff0d8214656e8c03a3703930728475ee2
ECLRAIN <irlawhddus@gmail.com>
2022-07-28 17:59:51 +0900

    스위치 로 달 브레이크는 위도적으로 안쓸수 있다

---
+++ day04/src/condition2/Test03.java
@@ -0,0 +1,50 @@
+package condition2;
+import java.lang.*;
+import java.util.Scanner;
+public class Test03 {
+	public static void main(String[] args) {
+	
+		int month=3;
+		
+		switch(month) {
+		case 1:
+			System.out.println("31일");
+			break;
+		case 2:
+			System.out.println("28일");
+			break;
+		case 3:
+			System.out.println("31일");
+			break;
+		case 4:
+			System.out.println("30일");
+			break;
+		case 5:
+			System.out.println("31일");
+			break;
+		case 6:
+			System.out.println("30일");
+			break;
+		case 7:
+			System.out.println("31일");
+			break;
+		case 8:
+			System.out.println("31일");
+			break;
+		case 9:
+			System.out.println("30일");
+			break;
+		case 10:
+			System.out.println("31일");
+			break;
+		case 11:
+			System.out.println("30일");
+			break;
+		case 12:
+			System.out.println("31일");
+			break;	
+            }
+	
+		
+	}
+}

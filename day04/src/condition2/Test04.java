73c22673c38102fbfe1a885e8711876a7b96300c
ECLRAIN <irlawhddus@gmail.com>
2022-07-28 18:21:11 +0900

    과제03

---
+++ day04/src/condition2/Test04.java
@@ -0,0 +1,13 @@
+package condition2;
+import java.lang.*;
+import java.util.Scanner;
+public class Test04 {
+	public static void main(String[] args) {
+		//손님이 입력하는 정보
+		Scanner sc = new Scanner(System.in);
+		System.out.println("연도를 입력하세요");
+		int year=sc.nextInt();
+		
+		sc.close();
+	}
+}

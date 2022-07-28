71ff101d528a4987d600e5f26ab15042b183f74d
ECLRAIN <irlawhddus@gmail.com>
2022-07-28 15:00:56 +0900

    중국집 계산 스캐너

---
+++ day04/src/io/Test04.java
@@ -0,0 +1,21 @@
+package io;
+import java.lang.*;
+import java.util.Scanner;
+public class Test04 {
+	public static void main(String[] args) {
+		//중국집 금액 계산 프로그램
+		//준비
+		
+		//사장님이 정하는 정보
+		int jja=5000;
+		int cham=6000;
+		//손님이 정하는 정보(입력)
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("자장면 개수를 입력하세요");
+		int jjaCount=sc.nextInt();
+		System.out.println("짬뽕 개수를 입력하세요");
+		int chamCount=sc.nextInt();
+		sc.close();
+	}
+}

9c1a8bd2478e42478c843ac5f8a213c108d9c727
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 11:22:42 +0900

    루프

--- day04/src/loop/Test03.java
+++ day04/src/loop/Test03.java
@@ -4,7 +4,7 @@ public class Test03 {
 	public static void main(String[] args) {
 		//반복수의 활용
 		//ex 1부터 100까지 출력
-		for(int i=100 ; i>=50 ; i=+1) {
+		for(int i=100 ; i>=50 ; i=1) {
 			System.out.println(i);
 		}
 	}
---
+++ day04/src/loop/Test04.java
@@ -0,0 +1,8 @@
+package loop;
+//import.java.lang.*;
+public class Test04 {
+	public static void main(String[] args) {
+		//사용자가 분과 초를 설정하면 해당하는 시간만큼의 메세지 출력 후 종료되는 프로그램을 구현
+		
+	}
+}

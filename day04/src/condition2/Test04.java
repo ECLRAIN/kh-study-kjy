e2e35ebca3812ada7730373252e4ae0b607a39e6
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 10:31:57 +0900

    과제3

--- day04/src/condition2/Test04.java
+++ day04/src/condition2/Test04.java
@@ -4,10 +4,29 @@ import java.util.Scanner;
 public class Test04 {
 	public static void main(String[] args) {
 		//손님이 입력하는 정보
-		Scanner sc = new Scanner(System.in);
-		System.out.println("연도를 입력하세요");
-		int year=sc.nextInt();
+		//윤년 판정
+		//준비
 		
-		sc.close();
+		int year=2022;
+		//계산
+		System.out.println(year%4);
+		System.out.println(year%100);
+		System.out.println(year%400);
+		
+		//boolean leap= true or false;
+		boolean leap;
+		if(year%4 !=0) {//year가 4의 배수가 아니면
+			leap=false;
+		}
+	//	if() {
+	//	System.out.println("윤년입니다");}
+		//else {
+		//System.out.println("평년입니다");}
+		
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("연도를 입력하세요");
+		//int year=sc.nextInt();
+		
+		//sc.close();
 	}
 }

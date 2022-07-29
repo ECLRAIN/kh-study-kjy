35ecc5c1a5276592d1f4c9a05f02ce7fbad0a130
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 18:20:37 +0900

    첫날에서 몇개씩 증가하는 예시

--- day05/src/day05/Test09.java
+++ day05/src/day05/Test09.java
@@ -9,7 +9,7 @@ public class Test09 {
 		for(int day=1 ; day<=30 ; day+=1) {
 		System.out.println(day+"일차 - "+pushup+"개");
 		total+=pushup;
-		pushup +=3; //안에서 반복해가는 증가량
+		pushup +=2; //안에서 반복해가는 증가량
 		
 		}
 	System.out.println("총 합계"+total);

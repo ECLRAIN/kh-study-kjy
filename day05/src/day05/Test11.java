1b167f0fbccd799572f2a919d2353785cb94ccd9
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 19:17:40 +0900

    과제 05

---
+++ day05/src/day05/Test11.java
@@ -0,0 +1,17 @@
+package day05;
+import java.lang.*;
+import java.util.Scanner;
+public class Test11 {
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+	//	System.out.println("숫자 1개를 입력하세요");
+		//firtst는 스캐너와 연동해서 끌어오면 해결
+		int first=5;
+		for(int i=1 ; i<=9 ; i+=1) {
+			System.out.println( first+ "x"+ i +"="  +"사용자가 정하는 숫자");
+		
+		
+		}
+		
+	}
+}

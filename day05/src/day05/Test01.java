97645655bcef282aa07b2a9ac0b70347a8670e37
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 13:44:16 +0900

    day05

--- day05/src/day05/Test01.java
+++ day05/src/day05/Test01.java
@@ -1,5 +1,26 @@
 package day05;
 
-public class Test01 {
+import java.util.Scanner;
 
+//import java.lang.*;
+public class Test01 {
+	public static void main(String[] args) {
+		//타이머 만들기
+		//2분 10초
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		int minute=sc.nextInt();
+		
+		System.out.println();
+		int sec=sc.nextInt();
+		sc.close();
+		//계산
+		int total= minute*60+sec;
+		System.out.println("total ="+total);
+		//출력
+		for(int i=total ; i>=0; i--) {
+		//System.out.println("2분 10초 남았습니다");
+		System.out.println(i/60+"분"+i%60+"초 남았습니다");
+		}
+	}
 }
--- day05/src/day05/Test02.java
+++ day05/src/day05/Test02.java
@@ -4,8 +4,21 @@ public class Test02 {
 	public static void main(String[] args) {
 		//q 1부터 100까지 홀수/짝수를 출력
 		//홀수
-		for(int i=1 ; i<=99 ; i+=2) {
+		for(int i=4 ; i<=99 ; i+=2) {
 			System.out.println("홀수 ="+i);
+		}
+		for(int i=1 ; i<=100 ; i++) {
+			if(i%2 ==1) {
+				System.out.println("홀수 = " +i);
+			}
+		}
+		
+		
+		for(int i=2 ; i<=100 ; i+=2) {
+			if(i%2==0) {
+				System.out.println("짝수 ="+i);
+			}
+			
 		}
 	}
 }

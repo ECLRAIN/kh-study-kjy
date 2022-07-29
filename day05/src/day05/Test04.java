c38f7422c58d6a6ae1b3df45a9ad22712cb7b5ca
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 15:02:23 +0900

    for int로 369

--- day05/src/day05/Test04.java
+++ day05/src/day05/Test04.java
@@ -2,7 +2,26 @@ package day05;
 import java.lang.*;
 public class Test04 {
 	public static void main(String[] args) {
-		for(int i= 1 ; i<=99 ; i++)
-		{System.out.println(i);}
+		//일의 자리 3|| 6|| 9|| //십의 자리에 
+		for(int i= 1 ; i<=99 ; i++) {
+			boolean ten =i /10==3 || i/10==6 || i/10==9;
+			boolean one =i %10==3 || i%10==6 || i%10==9;
+		//	System.out.println(ten);
+			if(ten || one) {
+				if(ten&&one) {
+					System.out.println("짝짝");
+				}
+				else {
+					System.out.println("짝");
+				}
+			}
+			else {
+				System.out.println(i);
+			}
+
+		}
+
+		
+
 	}
 }

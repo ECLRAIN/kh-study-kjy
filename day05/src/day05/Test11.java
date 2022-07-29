fc8a916d1aa083124669a15b33f6b07b7453dcb7
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 20:16:47 +0900

    과제0729

--- day05/src/day05/Test11.java
+++ day05/src/day05/Test11.java
@@ -1,17 +1,34 @@
 package day05;
+
 import java.lang.*;
 import java.util.Scanner;
+
 public class Test11 {
 	public static void main(String[] args) {
-		//Scanner sc = new Scanner(System.in);
-	//	System.out.println("숫자 1개를 입력하세요");
-		//firtst는 스캐너와 연동해서 끌어오면 해결
-		int first=5;
-		for(int i=1 ; i<=9 ; i+=1) {
-			System.out.println( first+ "x"+ i +"="  +"사용자가 정하는 숫자");
-		
-		
+		Scanner user = new Scanner(System.in);
+		System.out.println("구구단 첫단을 입력하세요");
+		int first = user.nextInt();
+
+		int count = 0;
+		int count2 = 0;
+		for (int i = 1; i <= 9; i += 1) {
+
+			System.out.println(first + "x" + i);
+			int num2 = user.nextInt(); // 사용자 입력한 정답
+
+			if (first * i == num2) {
+				count++;
+			} else if (first * i != num2)
+				count2++;
+			else {
+			}
 		}
-		
+
+		// if가 참일경우
+		// 예 5*1=5일경우 참이다
+		// ELES
+		user.close();
+		System.out.println("정답: " + count);
+		System.out.println("오답: " + count2);
 	}
 }

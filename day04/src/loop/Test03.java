7beb74cdfe800428a98a425e311a15f542b4b21c
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 11:15:37 +0900

    반복수의 활용

---
+++ day04/src/loop/Test03.java
@@ -0,0 +1,11 @@
+package loop;
+//import java.lang.*;
+public class Test03 {
+	public static void main(String[] args) {
+		//반복수의 활용
+		//ex 1부터 100까지 출력
+		for(int i=100 ; i>=50 ; i=+1) {
+			System.out.println(i);
+		}
+	}
+}

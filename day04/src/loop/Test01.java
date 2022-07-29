0397dec2d6a63080aca8163affec88fd634f5f6f
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 10:11:58 +0900

    반복1

---
+++ day04/src/loop/Test01.java
@@ -0,0 +1,12 @@
+package loop;
+import java.lang.*;
+public class Test01 {
+	public static void main(String[] args) {
+		//반복(loop)
+		//예) 화면에 hello를 10번 출력하세요
+		
+		for(int i=0 ; i<10; i=i+1) {
+			System.out.println("hi");
+		}
+	}
+}

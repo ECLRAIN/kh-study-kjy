2e0fca438a9bda072a3ba25182735fe1760ec762
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 13:43:59 +0900

    테스트2

---
+++ day05/src/day05/Test02.java
@@ -0,0 +1,11 @@
+package day05;
+//import java.lang.*;
+public class Test02 {
+	public static void main(String[] args) {
+		//q 1부터 100까지 홀수/짝수를 출력
+		//홀수
+		for(int i=1 ; i<=99 ; i+=2) {
+			System.out.println("홀수 ="+i);
+		}
+	}
+}

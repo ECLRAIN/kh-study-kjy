7992c273a997ce23510c74be259dbbecbf3f0fbc
ECLRAIN <irlawhddus@gmail.com>
2022-07-29 16:53:36 +0900

    반복문 합계

---
+++ day05/src/day05/Test08.java
@@ -0,0 +1,12 @@
+package day05;
+import java.lang.*;
+import java.util.Scanner;
+public class Test08 {
+	public static void main(String[] args) {
+		int total= 0;
+		for(int i=1 ; i<=10 ; i++) {
+			total+=i;
+		}
+		System.out.println("합계= "+total);
+	}
+}

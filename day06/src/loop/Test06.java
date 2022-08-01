2a228d055840603005c8bef14e0d9fe68e33fa4b
ECLRAIN <irlawhddus@gmail.com>
2022-08-01 13:38:17 +0900

    랜덤

---
+++ day06/src/loop/Test06.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class Test06 {
+	public static void main(String [] args) {
+		//랜덤
+		//-무엇이 나올지 예측이 되지 않는 값(주사위, 로또)
+		//확률적인 형태가 구현 가능함
+		//구현 가능한 방법
+		//1 Math.random()명령 사용
+		System.out.println(Math.random());
+		//2 Random 도구 사용
+		//3 SecureRandom 도구 사용
+	}
+}

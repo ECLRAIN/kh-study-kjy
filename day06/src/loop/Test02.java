26325a7f786c6b540c42d87e978824ac8b5b8551
ECLRAIN <irlawhddus@gmail.com>
2022-08-01 10:39:04 +0900

    for과while

---
+++ day06/src/loop/Test02.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Test02 {
+	public static void main(String[] args) {
+		//while 반복문 -특정 시점까지 반복하는 반복문
+		//for 반복문- 횟수나 범위가 주어졌을 때의 반복문
+		//ex : 1부터 10까지 출력
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		int n=1;
+		while(n<=10) {
+		System.out.println(n);
+		n++;
+		}
+		
+	}
+}

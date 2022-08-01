4776af7f12068d4b090f95a406fe017ee99b6da2
ECLRAIN <irlawhddus@gmail.com>
2022-08-01 09:54:31 +0900

    과제 풀이

---
+++ day06/src/loop/Test12.java
@@ -0,0 +1,25 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Test12 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("단수 : ");
+		int dan = sc.nextInt();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.print(dan + " X " + i + " = ");
+			int user = sc.nextInt();
+			if(dan * i == user) {
+				System.out.println("정답");
+			}
+			else {
+				System.out.println("오답");
+			}
+		}
+
+		sc.close();
+	}
+}

073eadb8faa49a4901535f44b0e6dbb3f8bda4ff
ECLRAIN <irlawhddus@gmail.com>
2022-08-01 10:16:47 +0900

    중첩 반복문 24시간의 시간 반복

---
+++ day06/src/loop/Test01.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//중첩 반복문
+		//1분간 시간을 출력
+		
+		for(int hour = 0; hour<24; hour++) {
+			for(int min=0; min<60; min++) {
+				for(int sec=0; sec<60; sec++) {
+					System.out.println();
+				}
+			}
+		}
+		
+		
+		
+		
+		
+	}
+}

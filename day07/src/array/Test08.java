6d61d099f55bf5a58914992bd16016fd19245023
ECLRAIN <irlawhddus@gmail.com>
2022-08-02 17:37:30 +0900

    배열문제풀이

---
+++ day07/src/array/Test08.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Test08 {
+	public static void main(String[]args) {
+		int[] data =new int[] {30, 20, 10, 50, 40};
+		//20과 40을 교체(맞교환은 안됨)
+		
+		int backup=data[1];
+		data[1]=data[4];
+		data[4]=backup;
+		
+		for(int i=0; i<data.length; i++) {
+			System.out.println(data[i]);
+		}
+		
+	}
+}
---
+++ day07/src/array/Test09.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Scanner;
+
+public class Test09 {
+	public static void main(String[] args) {
+
+		//배열 준비
+		int[] data = new int[5];
+
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i < data.length; i++) {
+			data[i] = sc.nextInt();
+		}
+		sc.close();
+
+		for(int i=0; i < data.length; i++) {
+			System.out.println("data = " + data[i]);
+		}
+
+		int total = 0;
+		for(int i=0; i < data.length; i++) {
+			total += data[i];
+		}
+
+		System.out.println("합계 = " + total);
+		double average = (double)total / data.length;
+		System.out.println("평균 = " + average);
+	}
+}

ace6417c317a38bfc1ac6c2c66cba358a48781e7
ECLRAIN <irlawhddus@gmail.com>
2022-08-04 14:46:08 +0900

    객체지향 출력에서 합계 평균

--- day09/src/oop/method3_1/Student.java
+++ day09/src/oop/method3_1/Student.java
@@ -3,27 +3,28 @@ package oop.method3_1;
 public class Student {
 	//멤버필드=데이터 저장소
 	String name;
-	double Korean;
-	double English;
-	double Math;
-	double 
-	
-	void setting(String name, double Korean, double English, double Math ) {
-		
-		this.name=name;
-		this.Korean=Korean;
-		this.English=English;
-		this.Math=Math;
-		
-	}
-		void print() {
-			int sum=this.Korean+this.English+this.Math;
-			
-			System.out.println("이름= "+this.name);
-			System.out.println("국어: "+this.Korean+"영어: "+this.English+"수학: "+this.Math);
-			System.out.println("총점: "+ this.sum);
-			System.out.println("평균: "+this.avg);
-		}
+	int korean;
+	int english;
+	int math;
 
-	
+	//멤버 메소드(method)
+	void setting(String name, int korean, int english, int math) {
+		this.name = name;
+		this.korean = korean;
+		this.english = english;
+		this.math = math;
+	}
+	void print() {
+		int total = this.korean + this.english + this.math; 
+		double average = total / 3.0;
+
+		System.out.println("<학생 정보>");
+		System.out.println("이름 : " + this.name);
+		System.out.println("국어 : " + this.korean+"점");
+		System.out.println("영어 : " + this.english + "점");
+		System.out.println("수학 : " + this.math+"점");
+		System.out.println("총점 : " + total+"점");
+		System.out.println("평균 : " + average+"점");
+	}
 }
+

5653f0aa513a984e3dced6fd089b7e068b90db37
ECLRAIN <irlawhddus@gmail.com>
2022-08-05 14:09:21 +0900

    setter2

--- day10/src/oop/setter2/Academy.java
+++ day10/src/oop/setter2/Academy.java
@@ -1,37 +1,64 @@
 package oop.setter2;
 
 public class Academy {
-	//필드
-	String name;
-	int time;
-	int price;
-	String type;
-	//setter method
-	//이름 지을때 앞에 set붙이자
-	void setName(String name) {
-		this.name=name;
-	}
-	void setSpeed(int time) {
-		this.time=time;	
-	}
-	void setPrice(int price) {
-		if(price<0) {
-		return;
+	//field
+		String name;
+		int time;
+		int price;
+		String type;
+		
+		//method
+		void setName(String name) {
+			this.name = name;
 		}
-		this.price=price;
-	}
-	void setType(String type){
-		switch (type) {
-		case "온라인";
-		case "오프라인";
-		case "
+		void setTime(int time) {
+//			[1]
+//			if(time > 0 && time % 30 == 0) {
+//				this.time = time;
+//			}
 			
-			break;
-
-		default:
-			break;
+//			[2]
+			if(time <= 0) {
+				return;
+			}
+			if(time % 30 != 0) {
+				return;
+			}
+			this.time = time;
 		}
-		this.type=type;
+		void setPrice(int price) {
+//			[1]
+//			if(price >= 0) {
+//				this.price = price;
+//			}
+			
+//			[2]
+			if(price < 0) {
+				return;
+			}
+			this.price = price;
+		}
+//		tip : 문자열은 비교 연산이 힘들다(안될 경우가 존재)
+		void setType(String type) {
+			switch(type) {
+			case "온라인":
+			case "오프라인":
+			case "혼합":
+				this.type = type;
+			}
+		}
+		void print() {
+			System.out.println("과정명 : " + this.name);
+			System.out.println("총시간 : " + this.time);
+			System.out.println("수강료 : " + this.price+"원");
+			System.out.println("방식 : " + this.type);
+		}
+		
+		//constructor
+		Academy(String name, int time, int price, String type){
+			this.setName(name);
+			this.setTime(time);
+			this.setPrice(price);
+			this.setType(type);
+		}
 	}
-	
-}
--- day10/src/oop/setter2/Test01.java
+++ day10/src/oop/setter2/Test01.java
@@ -1,5 +1,13 @@
 package oop.setter2;
 
 public class Test01 {
+	public static void main(String[] args) {
+		Academy a = new Academy("자바 마스터과정", 90, 1000000, "온라인");
+		Academy b = new Academy("파이썬 기초", 60, 600000, "온라인");
+		Academy c = new Academy("웹 개발자 양성과정", 120, 1200000, "오프라인");
 
+		a.print();
+		b.print();
+		c.print();
+	}
 }

99cd39b1ee59c15cc3513d5c98ce0d17024eee19
ECLRAIN <irlawhddus@gmail.com>
2022-08-08 16:48:28 +0900

    키워드9

---
+++ day11/src/oop/keyword9/Member.java
@@ -0,0 +1,8 @@
+package oop.keyword9;
+
+public class Member {
+	private String id;
+	private static String pw;
+	private String nickname;
+	
+}
---
+++ day11/src/oop/keyword9/Test01.java
@@ -0,0 +1,8 @@
+package oop.keyword9;
+import java.util.Scanner;
+public class Test01 {
+	public static void main(String []args) {
+	Scanner sc=new Scanner(System.in);
+		
+	}
+}

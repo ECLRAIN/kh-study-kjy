2a8a54a4eef26f4a73dcdbccf8c5ec2ce6e4b9cd
ECLRAIN <irlawhddus@gmail.com>
2022-08-08 16:49:11 +0900

    키워드4

---
+++ day11/src/oop/keyword4/Robot.java
@@ -0,0 +1,38 @@
+package oop.keyword4;
+
+public class Robot {
+	
+	//1회성 계산때 static을 쓴다
+	public static int squared(int value) {
+		return value * value;
+	}
+	
+	public static double bmi(double cm, double kg) {
+		double m = cm / 100;
+		return kg / (m * m);
+	}
+	
+	public static int subway(int year) {
+		int age = 2022 - year + 1;
+		if(age < 8 || age >= 65) {
+			return 0;
+		}
+		else if(age < 14){
+			return 450;
+		}
+		else if(age < 20) {
+			return 720;
+		}
+		else {
+			return 1250;
+		}
+	}
+	
+	public static double triangle(int width, int height) {
+		return width * height / 2.0;
+	}
+	
+	public static double circle(int radius) {
+		return 3.14 * radius * radius;
+	}
+}
---
+++ day11/src/oop/keyword4/Test01.java
@@ -0,0 +1,16 @@
+package oop.keyword4;
+
+public class Test01 {
+	public static void main(String []args) {
+		
+		System.out.println(11 * 11);//11곱하기 11
+		System.out.println(Robot.squared(11));//11의 제곱
+		
+		System.out.println(Robot.bmi(180, 80));//키180, 체중80의 BMI
+		System.out.println(Robot.subway(1999));//1999년생의 지하철요금
+		System.out.println(Robot.triangle(5, 7));
+		System.out.println(Robot.circle(7));
+		
+	}
+	
+}

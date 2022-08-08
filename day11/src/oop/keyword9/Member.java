36f100bd1755f639add32a915ba8699a23b21549
ECLRAIN <irlawhddus@gmail.com>
2022-08-08 17:03:59 +0900

    키워드9

--- day11/src/oop/keyword9/Member.java
+++ day11/src/oop/keyword9/Member.java
@@ -1,8 +1,37 @@
 package oop.keyword9;
 
 public class Member {
-	private String id;
-	private static String pw;
+	private static String id;
+	private String pw;
 	private String nickname;
+	public static String getId() {
+		return id;
+	}
+	public static void setId(String id) {
+		Member.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public Member(String id,String pw, String nickname) {
+		this.id=id;
+		this.pw = pw;
+		this.nickname = nickname;
+	}
+	
+	public void print() {
+		System.out.println(this.id);
+		System.out.println(this.nickname);
+	}
 	
 }
--- day11/src/oop/keyword9/Test01.java
+++ day11/src/oop/keyword9/Test01.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class Test01 {
 	public static void main(String []args) {
 	Scanner sc=new Scanner(System.in);
-		
+	
+	System.out.print("아이디 ");
+	String id= sc.next();
+	System.out.print("패스워드 ");
+	String pw=sc.next();
+	System.out.print("닉네임 ");
+	String nickname=sc.next();
+	sc.close();
+	
+	Member a=new Member(id,pw,nickname);
+	a.print();
 	}
 }

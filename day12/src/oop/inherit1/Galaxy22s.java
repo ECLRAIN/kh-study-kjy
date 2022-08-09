d391ec818b43dcf331f9545d0dcf9ad7b2502075
ECLRAIN <irlawhddus@gmail.com>
2022-08-09 09:57:42 +0900

    상속 도입이유

---
+++ day12/src/oop/inherit1/Galaxy22s.java
@@ -0,0 +1,17 @@
+package oop.inherit1;
+
+public class Galaxy22s {
+//	필드는 정보
+	private String color;
+	private String number;
+	//메소드
+	public void call() {
+		System.out.println("통화 기능");
+	}
+	public void camera() {
+		System.out.println("카메라 기능");
+	}
+	public void itunes() {
+		System.out.println("아이튠즈");
+	}
+}
---
+++ day12/src/oop/inherit1/IPhone13.java
@@ -0,0 +1,17 @@
+package oop.inherit1;
+
+public class IPhone13 {
+	//필드는 정보
+	private String color;
+	private String number;
+	//메소드
+	public void call() {
+		System.out.println("통화 기능");
+	}
+	public void camera() {
+		System.out.println("카메라 기능");
+	}
+	public void samsungPay() {
+		System.out.println("삼성페이");
+	}
+}

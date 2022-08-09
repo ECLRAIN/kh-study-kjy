a9084d99dd567fd12721c28e7f4ff53efe1430a0
ECLRAIN <irlawhddus@gmail.com>
2022-08-09 10:18:09 +0900

    상속 스타트

---
+++ day12/src/oop/inherit2/Phone.java
@@ -0,0 +1,17 @@
+package oop.inherit2;
+
+//상위 (슈퍼,부모) 클래스
+//여러 클래스에서 공통적으로 발생하는 코드(필드, 메소드)등을
+//객체를 만드는 것이 목적이 아님
+public class Phone {
+//공용 필드
+	private String color;
+	private String number;
+//	공용 메소드
+	public void call() {
+		System.out.println("통화 기능");
+	}
+	public void camera() {
+		System.out.println("카메라 기능");
+	}
+}
---
+++ day12/src/oop/inherit2/Test01.java
@@ -0,0 +1,18 @@
+package oop.inherit2;
+
+public class Test01 {
+	public static void main(String [] args) {
+		Galaxy22s a= new Galaxy22s();
+		
+		a.call();
+		a.camera();
+		a.samsungPay();
+		
+		IPhone13 b = new IPhone13();
+		b.call();
+		b.camera();
+		b.itunes();
+		
+		
+	}
+}

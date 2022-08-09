8135b6e1ce6daa8593a5413dcb1f26a2c68a00f8
ECLRAIN <irlawhddus@gmail.com>
2022-08-09 16:18:39 +0900

    상속6

---
+++ day12/src/oop/inherit6/GalaxyFold3.java
@@ -0,0 +1,10 @@
+package oop.inherit6;
+
+public class GalaxyFold3 extends Galaxy{
+	public GalaxyFold3(String number, String color) {
+		super(number,color);
+	}
+	public void iris() {
+		System.out.println("갤럭시폴드3 홍채인식 기능 실행");
+	}
+}
---
+++ day12/src/oop/inherit6/IPhone12.java
@@ -0,0 +1,8 @@
+package oop.inherit6;
+
+public class IPhone12 extends IPhone{
+	public  IPhone12(String number, String color) {
+		super(number,color);
+	}
+	public void
+}

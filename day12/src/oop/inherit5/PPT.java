7f94b10187086a30267370577b04586d787b6cf5
ECLRAIN <irlawhddus@gmail.com>
2022-08-09 16:18:28 +0900

    상속5

---
+++ day12/src/oop/inherit5/PPT.java
@@ -0,0 +1,35 @@
+package oop.inherit5;
+
+public class PPT extends File{
+	
+	//고유 필드
+	private int pageSize;
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		if(pageSize < 0) {
+			return;
+		}
+		this.pageSize = pageSize;
+	}
+	
+	//고유 메소드
+	public void information() {
+		System.out.println("<파일 정보>");
+		System.out.println("이름 : " + this.fileName);
+		System.out.println("크기 : " + this.fileSize+" bytes");
+		System.out.println("장수 : " + this.pageSize);
+	}
+	
+	//생성자
+	public PPT(String fileName, long fileSize) {
+		this(fileName, fileSize, 1);
+	}
+	public PPT(String fileName, long fileSize, int pageSize) {
+		super(fileName);
+		this.setFileSize(fileSize);
+		this.setPageSize(pageSize);
+	}
+	
+}

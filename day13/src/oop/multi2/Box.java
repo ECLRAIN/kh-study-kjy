e8e976218ef2f0dc2ef034e88afe7229c2e9a812
ECLRAIN <irlawhddus@gmail.com>
2022-08-11 09:32:03 +0900

    다중상속2

---
+++ day13/src/oop/multi2/Box.java
@@ -0,0 +1,12 @@
+package oop.multi2;
+
+public class Box {
+//	private Bus item;// 버스를 한 개 저장할 수 있는 자리를 만들겠다
+//	private Reserve item 전자제품을 한개 저장할 수 있는 자리를 만들겠다 (드론 킥보드)
+	//이 리서브를 상속받은 애만 자리를 만들수있다
+	// 트랜스포메이션을 하면 아무거나 한개 저장할  수 잇는 자리를 만들겠다
+	
+	public void setItem(Bus item) {
+		this.item=
+	}
+}
---
+++ day13/src/oop/multi2/Multibox.java
@@ -0,0 +1,6 @@
+package oop.multi2;
+//제너릭 타입
+//Generic type
+public class Multibox <E> {
+2208103398133101S
+}

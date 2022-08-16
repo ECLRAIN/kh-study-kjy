0066990ddba20ac633c1c7715a3e9f3efdbb73a9
ECLRAIN <irlawhddus@gmail.com>
2022-08-16 14:48:09 +0900

    숫자 형식 제어

---
+++ day16/src/api/util/date/Test04.java
@@ -0,0 +1,32 @@
+package api.util.date;
+
+import java.text.DecimalFormat;
+import java.text.Format;
+
+public class Test04 {
+	public static void main(String [] args) {
+		//숫자 형식 제어
+		//-0은 숫자가 없으면 그 자리에 0을 출력(자리를 고정하는 역할)		
+		//#은 해당 자리에 숫자가 없으면 출력하지 않음
+		//패턴을 인식하므로 콤마는 하나만 작성해도 된다
+		
+		int a=1234567890;
+		double b=1234.1234560;
+		Format f1= new DecimalFormat("0,000,000,000");
+		System.out.println(f1.format(a));
+		System.out.println(f1.format(b));
+		
+		System.out.println();
+		
+		Format f2= new DecimalFormat("#,###,###");
+		System.out.println(f2.format(a));
+		System.out.println(f2.format(b));
+		
+		System.out.println();
+		
+		Format f3= new DecimalFormat("#,##0.00");
+		System.out.println(f3.format(a));
+		System.out.println(f3.format(b));
+		System.out.println(f3.format(0.12));
+	}
+}

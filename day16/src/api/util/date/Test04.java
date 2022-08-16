6070b9eafaaee27e6cc107550e281da491b179e3
ECLRAIN <irlawhddus@gmail.com>
2022-08-16 14:48:45 +0900

    숫자 형식 제어 Format f1= new DecimalFormat("0,000,000,000");

--- day16/src/api/util/date/Test04.java
+++ day16/src/api/util/date/Test04.java
@@ -5,7 +5,7 @@ import java.text.Format;
 
 public class Test04 {
 	public static void main(String [] args) {
-		//숫자 형식 제어
+		//숫자 형식 제어 
 		//-0은 숫자가 없으면 그 자리에 0을 출력(자리를 고정하는 역할)		
 		//#은 해당 자리에 숫자가 없으면 출력하지 않음
 		//패턴을 인식하므로 콤마는 하나만 작성해도 된다

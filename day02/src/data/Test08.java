e5e1cb691e1b9676fa22e3fbb03ad034522a136e
ECLRAIN <irlawhddus@gmail.com>
2022-08-19 13:43:15 +0900

    2일자 시간 계산

--- day02/src/data/Test08.java
+++ day02/src/data/Test08.java
@@ -7,31 +7,10 @@ public class Test08 {
 		//-0분부터 9분까지 무료
 		//-10분부터 19분까지 1000원
 		//-이후 10분당 1000원씩 추가
+		int inhour=12, inmin=40;
+		int outhour=16,outmin=10;
 		
-		//준비-들어온 시간(시/분), 나간시간(시/분)
-		int inHour = 12;
-		int inMinute = 40;
-		int outHour = 16;
-		int outMinute = 10;
-		//시간 계산식
-		int inTime = inHour * 60 + inMinute;
-		int outTime = outHour * 60 + outMinute;
-		
-		int time = outTime - inTime;
-		System.out.println(time);
-		
-		//요금
-		int price = time / 10 * 1000;
-		
-		//시간
-		int hour = time / 60;
-		int minute = time % 60;
-		//출력
-		System.out.println("주차요금은 다음과 같습니다");
-		System.out.println(price);
-		
-		System.out.println("주차시간");
-		System.out.println(hour);
-		System.out.println(minute);
+		int inper=inmin*60;
+	
 	}
 }

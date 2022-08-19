60ebefcd2f9557ce634479a7810fbde46e87bf45
ECLRAIN <irlawhddus@gmail.com>
2022-08-19 13:42:36 +0900

    set 로또추첨

--- day17/src/api/util/collection2/Test01.java
+++ day17/src/api/util/collection2/Test01.java
@@ -4,23 +4,32 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Test01 {
-	public static void main (String []args) {
-	//set
-	//중복이 없는 저장소
-	//순서가 정해진 저장소
-	TreeSet a=new TreeSet();
-	Set<String> b= new TreeSet<>();
-	//추가 .add()
-	b.add("마리오");
-	b.add("마오");
-	b.add("마사오");
-	b.add("마리오");
-	
-	b.remove("마오");
-	System.out.println(b);
-	System.out.println(b.size());
-	System.out.println(b.contains("마오"));
-	//주의 .get이 없다
-	
-	}	
+	public static void main(String[] args) {
+		//Set
+		//- 중복이 없는 저장소
+		//- 순서가 정해진 저장소
+		
+		TreeSet a = new TreeSet();
+		TreeSet<String> b = new TreeSet<>();
+		Set<String> c = new TreeSet<>();
+		
+		//추가 - .add()
+		c.add("마리오");
+		c.add("루이지");
+		c.add("쿠파");
+		c.add("피오나");
+		c.add("루이지");
+		
+		//검색 - .contains()
+		System.out.println(c.contains("피카츄"));
+		System.out.println(c.contains("루이지"));
+		
+		//삭제 - .remove()
+		c.remove("마리오");
+		
+		System.out.println(c);
+		System.out.println(c.size());
+		
+		//주의 - .get()이 없다
+	}
 }

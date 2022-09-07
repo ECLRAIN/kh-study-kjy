6b6ed37263e291fe5e4fe2893e7e3e867ab6453d
ECLRAIN <irlwahddus@gmail.com>
2022-09-07 12:08:51 +0900

    포켓몬

--- springhome/src/main/java/com/kh/springhome/repository/PocketMonsterDao.java
+++ springhome/src/main/java/com/kh/springhome/repository/PocketMonsterDao.java
@@ -7,4 +7,5 @@ import com.kh.springhome.entity.PocketMonsterDto;
 public interface PocketMonsterDao {
 	void insert(PocketMonsterDto pocketMonsterDto); //매게변수
 	List<PocketMonsterDto> selectList();
+	PocketMonsterDto selectOne(int no);
 }

ae32c214ae6a4a02a3e768dcb0ca90d6b5ba3c2f
ECLRAIN <irlwahddus@gmail.com>
2022-09-13 13:38:20 +0900

    멤버인터셉터(회원이 아닌 존재가 회원 기능에 접근하는 것을 차단한다)

--- springhome/src/main/java/com/kh/springhome/interceptor/Memberinterceptor.java
+++ springhome/src/main/java/com/kh/springhome/interceptor/Memberinterceptor.java
@@ -1,5 +1,23 @@
 package com.kh.springhome.interceptor;
 
-public class Memberinterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+//이 클래스의 목적은 회원이 아닌 존재가 회원 기능에 접근하는 것을 차단한다
+@Component//단일 기능을 수행하는 도구
+//preHandle에서 true/false를 반환하면 통과/차단이 가능
+//설정클래스를 만들어서 
+public class Memberinterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		return true; // false
+	}
+
+	
+	
 }

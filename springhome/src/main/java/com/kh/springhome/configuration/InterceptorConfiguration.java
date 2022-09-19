5ee995c9ab1e3d51465444523a2d2b05392b43c0
ECLRAIN <irlwahddus@gmail.com>
2022-09-19 17:30:38 +0900

    소유주와 관리자만 삭제 수정 가능

--- springhome/src/main/java/com/kh/springhome/configuration/InterceptorConfiguration.java
+++ springhome/src/main/java/com/kh/springhome/configuration/InterceptorConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.kh.springhome.interceptor.AdminInterceptor;
+import com.kh.springhome.interceptor.MemberBoardOwnerCheckInterceptor;
+import com.kh.springhome.interceptor.MemberBoardPermissionCheckInterceptor;
 import com.kh.springhome.interceptor.MemberInterceptor;
 import com.kh.springhome.interceptor.TestInterceptor;
 
@@ -25,30 +27,37 @@ public class InterceptorConfiguration implements WebMvcConfigurer{
 	@Autowired
 	private TestInterceptor testInterceptor;
 	
+	@Autowired
+	private MemberBoardPermissionCheckInterceptor permissionCheckInterceptor;
+	
+	@Autowired
+	private MemberBoardOwnerCheckInterceptor ownerCheckInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		//registry에 추가하여 인터셉터가 작동하도록 설정
 		//(참고) 등록 코드 작성 순으로 실행됨
+		
 		//테스트 확인용 인터셉터
 		registry.addInterceptor(testInterceptor)
 					.addPathPatterns("/**");
+		
 		//회원용 인터셉터
 		registry.addInterceptor(memberInterceptor)
 					.addPathPatterns(//인터셉터가 감시할 주소
-							
 							"/pocketmon/**",//포켓몬스터 전부
 							"/music/detail",//음원 상세
 							"/member/**",//회원 전체
-							"/board/**" //게시판 전체 
-							
+							"/board/**"//게시판 전체
 					)
 					.excludePathPatterns(//위의 주소에서 제외할 주소
 							"/member/join*",//회원가입
 							"/member/login",//로그인
 							"/member/goodbye_result",//탈퇴완료
-							"/board/list", //게시판 목록
-							"board/detail" // 게시판 상세보기
+							"/board/list",//게시판 목록
+							"/board/detail"//게시판 상세보기
 					);
+		
 		//관리자용 인터셉터
 		registry.addInterceptor(adminInterceptor)
 					.addPathPatterns(//인터셉터가 감시할 주소
@@ -64,8 +73,19 @@ public class InterceptorConfiguration implements WebMvcConfigurer{
 						"/music/list",//음원 목록
 						"/music/detail"//음원 상세
 					);
+		
+		//관리자만 공지사항을 등록할 수 있도록 검사하는 인터셉터
+		registry.addInterceptor(permissionCheckInterceptor)
+					.addPathPatterns(
+							"/board/edit", 
+							"/board/write"
+					);
+		
+		//소유자 또는 관리자만 수정/삭제를 할 수 있도록 검사하는 인터셉터
+		registry.addInterceptor(ownerCheckInterceptor)
+					.addPathPatterns(
+							"/board/edit",
+							"/board/delete"
+					);
 	}
 }
- 
-
-
---
+++ springhome/src/main/java/com/kh/springhome/interceptor/MemberBoardPermissionCheckInterceptor.java
@@ -0,0 +1,52 @@
+package com.kh.springhome.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.kh.springhome.constant.SessionConstant;
+//db를 가기 위해 만든 것은 아님
+@Component
+public class MemberBoardPermissionCheckInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		//회원 검사기보다 늦게 실행 시킬 예정이므로 이 검사기에는 회원만 들어올수있게
+		//목표 관리자가 아닌 회원이 공지사항을 등록할려고 하면 차단
+		//1. 관리자인지 아닌지 검사 -관리자면 통과
+		//2. 1번이 아니라면 boardHead라는 파라미터 값이 "공지"이면 차단, 아니면 허용
+		//인터셉터는 허용과 차단을 해주긴 하지만 get과 post를 설정해주지 못 하는데
+		
+		
+		//만약 POST방식이 아니라면 통과
+				if(!request.getMethod().equals("POST")) {
+					return true;
+				}
+
+				//1
+				HttpSession session = request.getSession();
+				//String memberGrade = (String)session.getAttribute("mg");
+				String memberGrade = (String)session.getAttribute(SessionConstant.GRADE);
+				if(memberGrade.equals("관리자")) {
+					return true;
+				}
+
+				//2
+				String boardHead = request.getParameter("boardHead");
+				if(boardHead != null && !boardHead.equals("공지")) {
+					return true;
+				}
+
+				//나머지는 다 차단
+				response.sendError(403);
+				//response.sendError(HttpServletResponse.SC_FORBIDDEN);
+				return false;
+			}
+		}
+
+
+
